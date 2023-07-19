c318f5d3f827b4ed5e9fe5c2a9ec82c5e75e824d
Dinboa <bozyashka@icloud.com>
2023-07-19 09:35:56 +0300

    Initial Commit

---
+++ HomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork
+//
+//  Created by V on 19.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
